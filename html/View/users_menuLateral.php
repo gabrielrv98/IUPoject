ce3858c44340a1c4a9dfd8b83ea932797df34ea5
minimerlyn <gabrielrv98@gmail.com>
2019-11-26 01:07:47 +0100

    menu lateral now is lateral

--- html/View/users_menuLateral.php
+++ html/View/users_menuLateral.php
@@ -1,42 +1,5 @@
 
 	<nav>
-		
-		<head>
-			<link rel="stylesheet" type="text/css" href="../View/css/estilo.css">
-		</head>
-		
-
-		<div id="menuLateral" class="menuLateralCss">
-			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-
-			<a href='../Controller/EDIFICIO_Controller.php'>
-				<?php echo $strings['buildingManager']; ?>
-			</a>
-			<a href='../Controller/CENTRO_Controller.php'>
-				<?php echo $strings['centerManager']; ?>
-			</a>
-			<a href='../Controller/TITULACION_Controller.php'>
-				<?php echo $strings['TitulationManager']; ?>
-			</a>
-			<a href='../Controller/PROF_TITULACION_Controller.php'>
-				<?php echo $strings['P_TManager']; ?>
-			</a>
-			<a href='../Controller/PROFESOR_Controller.php'>
-				<?php echo $strings['profesorManager']; ?>
-			</a>
-			<a href='../Controller/PROF_ESPACIO_Controller.php'>
-				<?php echo $strings['PEmanager']; ?>
-			</a>
-			<a href='../Controller/ESPACIO_Controller.php'>
-				<?php echo $strings['spaceManager']; ?>
-			</a>
-			<a href='../Controller/USUARIOS_Controller.php'>
-				<?php echo $strings['Gestión Usuarios']; ?>
-			</a>
-		</div>
-		<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
-
-
 		<script>
 		function openNav() {
 		  document.getElementById("menuLateral").style.width = "250px";
@@ -46,5 +9,42 @@
 		  document.getElementById("menuLateral").style.width = "0";
 		}
 		</script>
+		<head>
+			<link rel="stylesheet" type="text/css" href="../View/css/estilo.css">
+		</head>
+		
+		<div id="menuLateralMain" class="menuLateralMain">
+			<div id="menuLateral" class="menuLateralCss">
+				<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+				<a href='../Controller/EDIFICIO_Controller.php'>
+					<?php echo $strings['buildingManager']; ?>
+				</a>
+				<a href='../Controller/CENTRO_Controller.php'>
+					<?php echo $strings['centerManager']; ?>
+				</a>
+				<a href='../Controller/TITULACION_Controller.php'>
+					<?php echo $strings['TitulationManager']; ?>
+				</a>
+				<a href='../Controller/PROF_TITULACION_Controller.php'>
+					<?php echo $strings['P_TManager']; ?>
+				</a>
+				<a href='../Controller/PROFESOR_Controller.php'>
+					<?php echo $strings['profesorManager']; ?>
+				</a>
+				<a href='../Controller/PROF_ESPACIO_Controller.php'>
+					<?php echo $strings['PEmanager']; ?>
+				</a>
+				<a href='../Controller/ESPACIO_Controller.php'>
+					<?php echo $strings['spaceManager']; ?>
+				</a>
+				<a href='../Controller/USUARIOS_Controller.php'>
+					<?php echo $strings['Gestión Usuarios']; ?>
+				</a>
+			</div>
+			<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+
+		</div>
+		
 
 	</nav>
