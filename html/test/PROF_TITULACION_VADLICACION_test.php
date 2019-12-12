98333601962ce64a195a5420cdc5a11ea0e015e8
minimerlyn <gabrielrv98@gmail.com>
2019-12-12 13:44:50 +0100

    PROF_TITULACION todas las validaciones

--- html/test/PROF_TITULACION_VADLICACION_test.php
+++ html/test/PROF_TITULACION_VADLICACION_test.php
@@ -299,8 +299,283 @@ function PROF_TITULACION_comprobar_year_test()
 	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
 }
 
+
+// function PROF_TITULACION_comprobar_ADD()
+// Valida:
+//		atributos OK
+//		atributos name y login mal
+
+function PROF_TITULACION_comprobar_ADD()
+{
+	global $ERRORS_array_test;
+// creo array de almacen de test individual
+	$PROF_TITULACION_array_test1 = array();
+
+	//sexo correcto
+	$PROF_TITULACION_array_test1['tipo'] = 'VALIDACION';
+	$PROF_TITULACION_array_test1['entidad'] = 'PROF_TITULACION';	
+	$PROF_TITULACION_array_test1['metodo'] = 'comprobar_atributos_ADD';
+	$PROF_TITULACION_array_test1['error'] = 'correcto';
+	$PROF_TITULACION_array_test1['error_esperado'] = 'true';
+	$PROF_TITULACION_array_test1['error_obtenido'] = '';
+	$PROF_TITULACION_array_test1['resultado'] = '';
+
+	// creo el modelo
+	$PROF_TITULACION = new PROF_TITULACION_Model('88516567D','name','2018-2019');
+	$PROF_TITULACION_array_test1['error_obtenido'] = $PROF_TITULACION->comprobar_atributos_ADD() == 1 ? 'true' : 'false';
+	if ($PROF_TITULACION_array_test1['error_obtenido'] === $PROF_TITULACION_array_test1['error_esperado'])
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'OK';
+	}
+	else
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'FALSE';
+	}
+
+	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
+
+	//sexo vacia
+	$PROF_TITULACION_array_test1['tipo'] = 'VALIDACION';
+	$PROF_TITULACION_array_test1['entidad'] = 'PROF_TITULACION';	
+	$PROF_TITULACION_array_test1['metodo'] = 'comprobar_atributos_ADD';
+	$PROF_TITULACION_array_test1['error'] = 'DNI y codigo titulacion erroneos';
+	$PROF_TITULACION_array_test1['error_esperado'] = 'dni-00010-dniError-codeTitulation-00060-alfNum-';
+	$PROF_TITULACION_array_test1['error_obtenido'] = '';
+	$PROF_TITULACION_array_test1['resultado'] = '';
+
+	// creo el modelo
+	$PROF_TITULACION = new PROF_TITULACION_Model('8851567D','na1me!','2018-2019');
+	$array = $PROF_TITULACION->comprobar_atributos_ADD();
+	//var_dump($array);
+	foreach ($array as $key ) {
+		foreach ($key as $key2) {
+			$result .= $key2.'-';
+		}
+	}
+	$PROF_TITULACION_array_test1['error_obtenido'] = $result;
+	if ($PROF_TITULACION_array_test1['error_obtenido'] === $PROF_TITULACION_array_test1['error_esperado'])
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'OK';
+	}
+	else
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'FALSE';
+	}
+
+	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
+
+	
+}
+
+// function PROF_TITULACION_comprobar_EDIT()
+// Valida:
+//		atributos OK
+//		atributos area y departamento mal
+
+function PROF_TITULACION_comprobar_EDIT()
+{
+	global $ERRORS_array_test;
+// creo array de almacen de test individual
+	$PROF_TITULACION_array_test1 = array();
+
+	//sexo correcto
+	$PROF_TITULACION_array_test1['tipo'] = 'VALIDACION';
+	$PROF_TITULACION_array_test1['entidad'] = 'PROF_TITULACION';	
+	$PROF_TITULACION_array_test1['metodo'] = 'comprobar_atributos_EDIT';
+	$PROF_TITULACION_array_test1['error'] = 'correcto';
+	$PROF_TITULACION_array_test1['error_esperado'] = 'true';
+	$PROF_TITULACION_array_test1['error_obtenido'] = '';
+	$PROF_TITULACION_array_test1['resultado'] = '';
+
+	// creo el modelo
+	$PROF_TITULACION = new PROF_TITULACION_Model('88516567D','name','2018-2019');
+
+	$PROF_TITULACION_array_test1['error_obtenido'] = $PROF_TITULACION->comprobar_atributos_EDIT() == 1 ? 'true' : 'false';
+	if ($PROF_TITULACION_array_test1['error_obtenido'] === $PROF_TITULACION_array_test1['error_esperado'])
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'OK';
+	}
+	else
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'FALSE';
+	}
+
+	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
+
+	//sexo vacia
+	$PROF_TITULACION_array_test1['tipo'] = 'VALIDACION';
+	$PROF_TITULACION_array_test1['entidad'] = 'PROF_TITULACION';	
+	$PROF_TITULACION_array_test1['metodo'] = 'comprobar_atributos_EDIT';
+	$PROF_TITULACION_array_test1['error'] = 'Codigo titulacin y año erroneos';
+	$PROF_TITULACION_array_test1['error_esperado'] = 'codeTitulation-00060-alfNum-ANHOACADEMICO-00110-anhoAcadCode-';
+	$PROF_TITULACION_array_test1['error_obtenido'] = '';
+	$PROF_TITULACION_array_test1['resultado'] = '';
+
+	// creo el modelo
+	$PROF_TITULACION = new PROF_TITULACION_Model('88516567D','name1!','2018019');
+	$array = $PROF_TITULACION->comprobar_atributos_EDIT();
+	//var_dump($array);
+	foreach ($array as $key ) {
+		foreach ($key as $key2) {
+			$result .= $key2.'-';
+		}
+	}
+	$PROF_TITULACION_array_test1['error_obtenido'] = $result;
+	if ($PROF_TITULACION_array_test1['error_obtenido'] === $PROF_TITULACION_array_test1['error_esperado'])
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'OK';
+	}
+	else
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'FALSE';
+	}
+
+	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
+
+	
+}
+
+// function PROF_TITULACION_comprobar_DELETE()
+// Valida:
+//		atributos OK
+//		atributos name y login mal
+
+function PROF_TITULACION_comprobar_DELETE()
+{
+	global $ERRORS_array_test;
+// creo array de almacen de test individual
+	$PROF_TITULACION_array_test1 = array();
+
+	//sexo correcto
+	$PROF_TITULACION_array_test1['tipo'] = 'VALIDACION';
+	$PROF_TITULACION_array_test1['entidad'] = 'PROF_TITULACION';	
+	$PROF_TITULACION_array_test1['metodo'] = 'comprobar_atributos_DELETE';
+	$PROF_TITULACION_array_test1['error'] = 'correcto';
+	$PROF_TITULACION_array_test1['error_esperado'] = 'true';
+	$PROF_TITULACION_array_test1['error_obtenido'] = '';
+	$PROF_TITULACION_array_test1['resultado'] = '';
+
+	// creo el modelo
+	$PROF_TITULACION = new PROF_TITULACION_Model('88516567D','codeTit','');
+
+	$PROF_TITULACION_array_test1['error_obtenido'] = $PROF_TITULACION->comprobar_atributos_DELETE() == 1 ? 'true' : 'false';
+	if ($PROF_TITULACION_array_test1['error_obtenido'] === $PROF_TITULACION_array_test1['error_esperado'])
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'OK';
+	}
+	else
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'FALSE';
+	}
+
+	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
+
+	//sexo vacia
+	$PROF_TITULACION_array_test1['tipo'] = 'VALIDACION';
+	$PROF_TITULACION_array_test1['entidad'] = 'PROF_TITULACION';	
+	$PROF_TITULACION_array_test1['metodo'] = 'comprobar_atributos_DELETE';
+	$PROF_TITULACION_array_test1['error'] = 'DNI y codigo de la titulacion erroneos';
+	$PROF_TITULACION_array_test1['error_esperado'] = 'dni-00010-dniError-codeTitulation-00060-alfNum-';
+	$PROF_TITULACION_array_test1['error_obtenido'] = '';
+	$PROF_TITULACION_array_test1['resultado'] = '';
+
+	// creo el modelo
+	$PROF_TITULACION = new PROF_TITULACION_Model('81657D','code!!','');
+	$array = $PROF_TITULACION->comprobar_atributos_DELETE();
+	//var_dump($array);
+	foreach ($array as $key ) {
+		foreach ($key as $key2) {
+			$result .= $key2.'-';
+		}
+	}
+	$PROF_TITULACION_array_test1['error_obtenido'] = $result;
+	if ($PROF_TITULACION_array_test1['error_obtenido'] === $PROF_TITULACION_array_test1['error_esperado'])
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'OK';
+	}
+	else
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'FALSE';
+	}
+
+	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
+}
+
+
+// function PROF_TITULACION_comprobar_RellenaDatos()
+// Valida:
+//		atributos OK
+//		atributos name y login mal
+
+function PROF_TITULACION_comprobar_RellenaDatos()
+{
+	global $ERRORS_array_test;
+// creo array de almacen de test individual
+	$PROF_TITULACION_array_test1 = array();
+
+	//sexo correcto
+	$PROF_TITULACION_array_test1['tipo'] = 'VALIDACION';
+	$PROF_TITULACION_array_test1['entidad'] = 'PROF_TITULACION';	
+	$PROF_TITULACION_array_test1['metodo'] = 'comprobar_atributos_RellenaDatos';
+	$PROF_TITULACION_array_test1['error'] = 'correcto';
+	$PROF_TITULACION_array_test1['error_esperado'] = 'true';
+	$PROF_TITULACION_array_test1['error_obtenido'] = '';
+	$PROF_TITULACION_array_test1['resultado'] = '';
+
+	// creo el modelo
+	$PROF_TITULACION = new PROF_TITULACION_Model('88516567D','codeTit','');
+
+	$PROF_TITULACION_array_test1['error_obtenido'] = $PROF_TITULACION->comprobar_atributos_RellenaDatos() == 1 ? 'true' : 'false';
+	if ($PROF_TITULACION_array_test1['error_obtenido'] === $PROF_TITULACION_array_test1['error_esperado'])
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'OK';
+	}
+	else
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'FALSE';
+	}
+
+	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
+
+	//sexo vacia
+	$PROF_TITULACION_array_test1['tipo'] = 'VALIDACION';
+	$PROF_TITULACION_array_test1['entidad'] = 'PROF_TITULACION';	
+	$PROF_TITULACION_array_test1['metodo'] = 'comprobar_atributos_RellenaDatos';
+	$PROF_TITULACION_array_test1['error'] = 'DNI y codigo de la titulacion erroneos';
+	$PROF_TITULACION_array_test1['error_esperado'] = 'dni-00010-dniError-codeTitulation-00002-toolong-';
+	$PROF_TITULACION_array_test1['error_obtenido'] = '';
+	$PROF_TITULACION_array_test1['resultado'] = '';
+
+	// creo el modelo
+	$PROF_TITULACION = new PROF_TITULACION_Model('81657D','codeasdasdasdasdsad','');
+	$array = $PROF_TITULACION->comprobar_atributos_RellenaDatos();
+	//var_dump($array);
+	foreach ($array as $key ) {
+		foreach ($key as $key2) {
+			$result .= $key2.'-';
+		}
+	}
+	$PROF_TITULACION_array_test1['error_obtenido'] = $result;
+	if ($PROF_TITULACION_array_test1['error_obtenido'] === $PROF_TITULACION_array_test1['error_esperado'])
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'OK';
+	}
+	else
+	{
+		$PROF_TITULACION_array_test1['resultado'] = 'FALSE';
+	}
+
+	array_push($ERRORS_array_test, $PROF_TITULACION_array_test1);
+}
+
+
 	PROF_TITULACION_comprobar_dni_test();
 	PROF_TITULACION_comprobar_titulacion_test();
 	PROF_TITULACION_comprobar_year_test();
+
+	PROF_TITULACION_comprobar_ADD();
+	PROF_TITULACION_comprobar_EDIT();
+	PROF_TITULACION_comprobar_DELETE();
+	PROF_TITULACION_comprobar_RellenaDatos();
 
 ?>
